92e4454469a355fd89f4112b55e58c7e31c4b06c
fjuza1 <fjuza@ditec.sk>
2024-10-22 09:19:23 +0200

    filterinfo in skillsview

--- src/js/Views/skillsView.js
+++ src/js/Views/skillsView.js
@@ -7,6 +7,12 @@ class SkillsView extends View {
 		let valNow;
 		let width;
 		let color
+		const filterinfo = `          
+			<button type="button" class="btn btn-link"> 
+				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+				<path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+				</svg>
+			</button>`
 		data.forEach(barArea => {
 			switch (barArea.level) {
 				case 'Beginner':
@@ -47,7 +53,7 @@ class SkillsView extends View {
 		   `);
 
 		});
-		return'<h3>Skills</h3>' + html.join('');
+		return'<h3>Skills</h3>' + filterinfo + html.join('');
 	}
 	_sortingSkills(options) {
 		const sortFunctions = {
