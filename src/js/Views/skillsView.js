4c0887144446e8b36741c2bca01c996b986d0021
fjuza1 <fjuza@ditec.sk>
2024-10-22 10:20:30 +0200

    inview? category as sort added

--- src/js/Views/skillsView.js
+++ src/js/Views/skillsView.js
@@ -13,7 +13,7 @@ class SkillsView extends View {
 				<path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
 				</svg>
 			</button>`
-		const exportBtn = `
+        const exportBtn = `
 		    <button type="button" class="btn btn-link" tabindex="0" data-toggle="tooltip" title="Export"> 
 				<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
 				<path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1z"/>
@@ -64,7 +64,8 @@ class SkillsView extends View {
     _sortingSkills(options) {
         const sortFunctions = {
             expertise: (a, b) => options.order === 'asc' ? a.levelNumber - b.levelNumber : b.levelNumber - a.levelNumber,
-            name: (a, b) => options.order === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+            name: (a, b) => options.order === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name),
+            category: (a, b) => options.order === 'asc' ? a.category.localCompare(b.category) : b.category.localCompare(a.category)
         };
 
         return options.array.sort(sortFunctions[options.sortBy]);
--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
 export const EXPERT_LEVEL = ['Beginner','Basic','Skillful','Advanced','Expert'];
 export const EXPERT_NUM = [1,2,3,4,5];
+export const CATEGORIES = ['Tool','Skill','Language','Other']
