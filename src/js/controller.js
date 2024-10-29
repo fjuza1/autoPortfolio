9d7d96ec6394479980615a708c5b19cc7bedf981
fjuza1 <fjuza@ditec.sk>
2024-10-29 14:42:04 +0100

    Revert "new name and 1 vioew"
    This reverts commit 456f45d0ddf07a23c1fa6515f4cb956eb62bc4c3.

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,17 +3,16 @@ import 'regenerator-runtime/runtime';
 import {async} from 'regenerator-runtime';
 import * as model from './model.js';
 import * as help from './helpers.js'
-import popupView from './Views/popoutView.js';
-import designView from './Views/designView.js';
+import popupView from './Views/PopoutView.js';
+import design from './Views/DesignView.js';
 import skillsView from './Views/skillsView.js';
-import skillsExportView from './Views/skillsExportView.js';
 const controllNavBar = () => {
-   designView.addHandlerHover(designView.handleHover)
-   designView.addHandlerNavObserver()
-   designView.addScrollIntoHandler(designView.scrollIntoSection)
+   design.addHandlerHover(design.handleHover)
+   design.addHandlerNavObserver()
+   design.addScrollIntoHandler(design.scrollIntoSection)
 }
 const controlSections = () => {
-   designView.addRevealSectionObserver()
+   design.addRevealSectionObserver()
 }
 const controllSkillDisplay = () => {
    skillsView._render(skillsView._skillBarDisplay(model.state.skills))
@@ -23,7 +22,9 @@ const controllResults = () =>{
    skillsView._data.array = model.state.skills;
    skillsView._renderSpinner();
    const sorted = skillsView._sortingSkills(skillsView._data)
-   help.timeout(() => {skillsView._render(skillsView._skillBarDisplay(sorted))});
+   help.timeout(() => {
+      skillsView._render(skillsView._skillBarDisplay(sorted))
+   });
 }
 const controllResultsReset = () =>{
    const originalArraySkills = model.original.skills;
