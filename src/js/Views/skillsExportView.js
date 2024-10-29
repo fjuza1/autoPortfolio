456f45d0ddf07a23c1fa6515f4cb956eb62bc4c3
fjuza1 <fjuza@ditec.sk>
2024-10-29 13:01:29 +0100

    new name and 1 vioew

---
+++ src/js/Views/skillsExportView.js
@@ -0,0 +1,11 @@
+import View from './View.js';
+class SkillsExportView extends View {
+    constructor(){
+        super();
+    }
+    _form = document.getElementById('export');
+    _revealExportContainer(){
+
+    }
+}
+export default new SkillsExportView();
--- src/js/controller.js
+++ src/js/controller.js
@@ -3,16 +3,17 @@ import 'regenerator-runtime/runtime';
 import {async} from 'regenerator-runtime';
 import * as model from './model.js';
 import * as help from './helpers.js'
-import popupView from './Views/PopoutView.js';
-import design from './Views/DesignView.js';
+import popupView from './Views/popoutView.js';
+import designView from './Views/designView.js';
 import skillsView from './Views/skillsView.js';
+import skillsExportView from './Views/skillsExportView.js';
 const controllNavBar = () => {
-   design.addHandlerHover(design.handleHover)
-   design.addHandlerNavObserver()
-   design.addScrollIntoHandler(design.scrollIntoSection)
+   designView.addHandlerHover(designView.handleHover)
+   designView.addHandlerNavObserver()
+   designView.addScrollIntoHandler(designView.scrollIntoSection)
 }
 const controlSections = () => {
-   design.addRevealSectionObserver()
+   designView.addRevealSectionObserver()
 }
 const controllSkillDisplay = () => {
    skillsView._render(skillsView._skillBarDisplay(model.state.skills))
@@ -22,9 +23,7 @@ const controllResults = () =>{
    skillsView._data.array = model.state.skills;
    skillsView._renderSpinner();
    const sorted = skillsView._sortingSkills(skillsView._data)
-   help.timeout(() => {
-      skillsView._render(skillsView._skillBarDisplay(sorted))
-   });
+   help.timeout(() => {skillsView._render(skillsView._skillBarDisplay(sorted))});
 }
 const controllResultsReset = () =>{
    const originalArraySkills = model.original.skills;
