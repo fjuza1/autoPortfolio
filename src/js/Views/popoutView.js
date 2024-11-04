6e21ed2253eac8e6d0cd46a602483db9d102b48e
fjuza1 <fjuza@ditec.sk>
2024-11-04 12:32:56 +0100

    ne ideas

--- src/js/Views/popoutView.js
+++ src/js/Views/popoutView.js
@@ -20,6 +20,8 @@ class PopupView {
         const multi = [...this._multiCollapse].some(el => el.contains(target));
         this._multiCollapse.forEach(el => !multi && !button ? el.classList.remove('show') : '');
     }
+    _createModal(){}
+    _deleteModal(){}
     _addHandlerShowSection() {
         this._skillBtnGroup.addEventListener('click', this._toggleSection.bind(this));
     }
--- src/js/Views/slidesView.js
+++ src/js/Views/slidesView.js
@@ -4,7 +4,6 @@ class SlidesView {
     _prevBtn = document.querySelector('.carousel-control-prev-icon');
     _nextBtn = document.querySelector('.carousel-control-next-icon');
     _slideIndicators = document.querySelector('.carousel-indicators');
-    _currentSlide = 0;
 
     _removeSlideClass (){
         this._slides.forEach(slide => slide.classList.remove('active'));
@@ -12,8 +11,15 @@ class SlidesView {
     _removeIndicatorsClass () {
         this._slideIndicators.forEach(indicator => indicator.classList.remove('active'));
     }
+    _goto(index) {
+        this._removeSlideClass();
+        this._removeIndicatorsClass();
+        this._slides(index).classList.add('active');
+        this._slideIndicators(index).classList.add('active');
+    }
     goToSlide(slide) {
-        let initialSlide = 0;
+        let init = 0;
+        
     }
     nextSlide() {}
     prevSlide() {}
