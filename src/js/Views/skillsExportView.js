78299af3bfa6ff58b05e3c7bd1de6a590ddb2f23
fjuza1 <fjuza@ditec.sk>
2024-11-06 10:27:48 +0100

    reveal is done

--- src/js/Views/skillsExportView.js
+++ src/js/Views/skillsExportView.js
@@ -3,19 +3,20 @@ import { toFile }  from '../model.js';
 class SkillsExportView extends View {
     _form = document.querySelector('.exportActivities');
     _parentElement = document.getElementById('exportModal')
+    _fileType = document.querySelector('.content.d-none');
     constructor(){
         super();
+        this._form.addEventListener('change', () => {
+            this._revealExportContainer();
+        });
     }
     _revealExportContainer(){
-
+        if(this._fileType.classList.contains('d-none')) this._fileType.classList.remove('d-none');
     }
     export(options){
         this._formData = options
         const fileErrors = toFile(options)
         if(!fileErrors) return;
-        this.fileErrors = fileErrors;
-        if(fileErrors && fileErrors.some(err=>err.message.toLowerCase().includes('type')) && fileErrors.length === 1) this._outlineErrors(fileError.find(err=>err.message.toLowerCase().includes('type')))
-        if(fileErrors && fileErrors.length > 0) this._outlineErrors(this.fileErrors);
     }
 }
 export default new SkillsExportView();
