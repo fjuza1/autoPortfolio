62f6cc247ce313ca95b8f5160ac4a4a8ce700547
fjuza1 <fjuza@ditec.sk>
2024-11-06 15:29:36 +0100

    contact partdone

--- src/js/Views/contactView.js
+++ src/js/Views/contactView.js
@@ -1,6 +1,11 @@
 import View from './View.js';
 import sendMail from '../helpers.js';
 class ContactView extends View {
+    _parentElement = document.getElementById('contactForm')
+    _getRequiredFields (){
+        [...this._parentElement]
+        .map(reqField => reqField.classList.contains('required'))
+    }
     _isRobot(){}
     _sendMail(options){
         const {name, email, subject, message} = options
