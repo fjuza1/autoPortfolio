8c31d080ba89e38702e7ee31e193bf4d5506efab
fjuza1 <fjuza@ditec.sk>
2024-11-12 11:51:34 +0100

    jum[ping works

--- src/js/Views/slidesView.js
+++ src/js/Views/slidesView.js
@@ -3,37 +3,36 @@ class SlidesView {
     _slides = document.querySelectorAll('.carousel-item');
     _prevBtn = document.querySelector('.carousel-control-prev');
     _nextBtn = document.querySelector('.carousel-control-next');
-    _slideIndicators = document.querySelector('.carousel-indicators').children;
-
+    _slideIndicatorsContainer = document.querySelector('.carousel-indicators')
+    _slideIndicators = this._slideIndicatorsContainer.children;
     _renderSlides (){
         // Implement logic to render slides and slide indicators here
-        this._slides = document.querySelector('.carousel-inner')
-        this._prevBtn = document.querySelector('.carousel-control-prev-icon');
-        this._nextBtn = document.querySelector('.carousel-control-next-icon');
-        this._slideIndicators = document.querySelector('.carousel-indicators');
+
         // end
     }
     _removeSlideClass (){
         this._slides.forEach(slide => slide.classList.remove('active'));
     }
     _removeIndicatorsClass () {
-        this._slideIndicators.forEach(indicator => indicator.classList.remove('active'));
+        [...this._slideIndicators].forEach(indicator => indicator.classList.remove('active'));
     }
-    _goto(index = index - 1) {
+    _goto(index) {
         this._removeSlideClass();
         this._removeIndicatorsClass();
-        this._slides(index).classList.add('active');
-        this._slideIndicators(index).classList.add('active');
+        this._slides[index].classList.add('active');
+        this._slideIndicators[index].classList.add('active');
     }
-    goToSlide(slide) {
-        let init = 0;
-
+    goToSlide(e) {
+        const dataset = e.target.dataset.bsSlideTo;
+        this._goto(dataset)
     }
     nextSlide() {}
     prevSlide() {}
     _animateSlides() {
         // TODO Implement logic to animate slides here
     }
-    addSlideHandler(handler) {}
+    addSlideHandler(handler) {
+        this._slideIndicatorsContainer.addEventListener('click', handler);
+    }
 }
 export default new SlidesView();
