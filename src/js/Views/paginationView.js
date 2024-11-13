de2082f746c8b8353c09d8c39bf47937815e7197
fjuza1 <phil.juza1@gmail.com>
2024-11-13 19:40:57 +0100

    how to use addHandlerPagination

--- src/js/Views/paginationView.js
+++ src/js/Views/paginationView.js
@@ -1,6 +1,13 @@
 class PaginationView{
     _parentElement = document.querySelector('.pagination');
-
+    _pageStart = 0;
+    addHandlerPagination(handler) {
+        this._parentElement.addEventListener('click', (e) => {
+            if(e.target.tagName === 'SPAN' && e.target.id === 'loadMore') {
+                handler(_data.currentPage++);
+            }
+        });
+    }
     _getNumPages(_data) {
         return Math.ceil(_data.data.length / _data.resPerPage);
     }
@@ -10,7 +17,7 @@ class PaginationView{
         const numpages = this._getNumPages(_data);
         // Page 1, and there are other pages
         if(curPage < numpages) markup = `
-            <span class="d-block text-primary text-center py-2 cursor-pointer">Load more</span>
+            <span class="d-block text-primary text-center py-2 cursor-pointer" id="loadMore" >Load more</span>
         `
         // If last page, no pagination needed
         if(curPage === numpages && numpages > 1) {
