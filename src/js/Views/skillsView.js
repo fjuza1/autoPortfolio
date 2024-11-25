8d8cab1645bc003169d3ae70885c91b2f8006267
fjuza1 <104192489+fjuza1@users.noreply.github.com>
2024-11-25 11:07:04 +0100

    shoulkd only search on inpuit

--- src/js/Views/skillsView.js
+++ src/js/Views/skillsView.js
@@ -59,7 +59,7 @@ class SkillsView extends View {
         // TODO Auto return immediately to stop anything else be put
     }
     _addFilterSkillsHandler(handler) {
-        ['change','keyup', 'paste'].forEach(ev => this._form.addEventListener(ev, (e) => {
+        ['change','input', 'paste'].forEach(ev => this._form.addEventListener(ev, (e) => {
             const name = e.target.getAttribute('name')
             if (!name) return;
             if (ALLOWED_FILTER_SKILLS.includes(name)) {
