e61737d2d5fde7e0f161ab75c2ebee0c888a4a4a
fjuza1 <phil.juza1@gmail.com>
2025-01-04 14:06:11 +0100

    promise no need for async

--- src/js/model.js
+++ src/js/model.js
@@ -230,8 +230,8 @@ export const toFile = async (options) => {
                 break;
             case EXPORT_WHITELIST[1]:
                 content = toJSON(array)
-                const contentJSON = await isJSON(content);
-                if(contentJSON === false) return;
+                const contentJSON = isJSON(content);
+                if(!contentJSON) return;
                 textType = {
                     type: `${JSON_TYPE}; ${DEFAULT_ENCODING}`
                 }
@@ -285,7 +285,7 @@ export const filterSkills = function(options) {
 export const sortingSkills = function(options) {
     let { array, sortBy, order } = options;
     const skills = state.skills
-    state.skills.filteredSkills ? array = state.skills.filteredSkills : skills;
+   skills.filteredSkills ? array = skills.filteredSkills : skills;
     const sortFunctions = {
         expertise: (a, b) => order === 'asc' ? a.levelNumber - b.levelNumber : b.levelNumber - a.levelNumber,
         name: (a, b) => order === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name),
